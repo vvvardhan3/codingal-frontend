816e0929bfa0c7b534e48c8d34fdd2381815ea29
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-12-21 00:03:24 +0530

    Integrated navbar with timer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import VerticalLine from "./VerticalLine";
 
 function Navbar() {
-  const imageUrl =
-    "https://fastly.codingal.com/images/logos/logos-christmas/logo-with-text.svg";
+  const [seconds, setSeconds] = useState(600);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      } else {
+        clearInterval(interval);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [seconds]);
+
+  const formatTime = () => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${
+      remainingSeconds < 10 ? `0${remainingSeconds}` : remainingSeconds
+    }`;
+  };
 
   return (
     <>
-      <div className="sticky top-0 flex h-16 z-30 shadow bg-white">
-        <div className="z-20 flex items-center justify-between w-full px-5 cursor-pointer">
-          <img className="h-8 w-28" src={imageUrl} alt="logo" />
-          <div>
-            <button className="px-4 py-2 rounded-md text-white bg-custom-orange cursor-pointer">
-              End class
-            </button>
-          </div>
+      <div className="sticky top-0 flex h-16 z-30 w-fill bg-white justify-between items-center">
+        <div className="z-20 flex items-center w-full px-5 cursor-pointer space-x-4">
+          <img
+            className="h-10 rounded-md"
+            src={require("../Logo.jpg")}
+            alt="logo"
+          />
+          <VerticalLine />
+          <div className="text-gray-500 text-sm font-bold">Trial Lesson [Grade 1-3]</div>
         </div>
+        <div className="text-gray-500 font-bold">{formatTime()}</div>
+        <button className=" m-5 px-2 w-[120px] py-2 rounded-md text-white bg-custom-orange cursor-pointer">
+          End class
+        </button>
       </div>
     </>
   );
---
+++ src/components/VerticalLine.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const VerticalLine = () => {
+  return <div className="h-16 w-[2px] bg-gray-200 mx-auto"></div>;
+};
+
+export default VerticalLine;
