e103c3543c1fb59b696369127f2ea548bda9bd7b
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-12-20 21:34:42 +0530

    components created

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import TimerModal from './TimerModal'
+
+function Navbar() {
+  return (
+    <>
+     <div>Navbar</div>
+     <TimerModal/>
+    </>
+   
+    
+  )
+}
+
+export default Navbar
---
+++ src/components/TimerModal.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TimerModal() {
+  return (
+    <div>TimerModal</div>
+  )
+}
+
+export default TimerModal
