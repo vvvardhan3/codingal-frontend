59631635e8df08d8e81ed97e81ce20e81bff795a
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-12-20 22:31:15 +0530

    Navbar integrated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,23 @@
-import React from 'react'
-import TimerModal from './TimerModal'
+import React from "react";
 
 function Navbar() {
+  const imageUrl =
+    "https://fastly.codingal.com/images/logos/logos-christmas/logo-with-text.svg";
+
   return (
     <>
-     <div>Navbar</div>
-     <TimerModal/>
+      <div className="sticky top-0 flex h-16 z-30 shadow bg-white">
+        <div className="z-20 flex items-center justify-between w-full px-5 cursor-pointer">
+          <img className="h-8 w-28" src={imageUrl} alt="logo" />
+          <div>
+            <button className="px-4 py-2 rounded-md text-white bg-custom-orange cursor-pointer">
+              End class
+            </button>
+          </div>
+        </div>
+      </div>
     </>
-   
-    
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-orange': '#ff5a43',
+      }
+    },
   },
   plugins: [],
 }
