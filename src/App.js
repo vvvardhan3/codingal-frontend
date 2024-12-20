fac41eb8f649a851671e2a87d24abbe1b7d02811
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-12-20 21:34:18 +0530

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Posts from './components/Posts';
+
+function App() {
+  return (
+   <>
+   <Navbar/>
+   <Posts/>
+   </>
+  );
+}
+
+export default App;
